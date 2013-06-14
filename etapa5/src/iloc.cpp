6de1a541f1247dcc4f51d272828a056d902e0068
Lucas Lazari <lucaslazari86@gmail.com>
2013-06-14 01:07:13 -0300

    renomando classe iloc

---
+++ etapa5/src/iloc.cpp
@@ -0,0 +1,34 @@
+#include "iloc_node.h"
+
+ILOC::ILOC(int type, Symbol *src1, Symbol *src2, Symbol *dst1, Symbol *dst2) {
+
+    this->type = type;
+
+    this->src1 = src1;
+    this->src2 = src2;
+    this->dst1 = dst1;
+    this->dst2 = dst2;
+
+    this->prev = 0;
+    this->next = 0;
+
+}
+
+static void join(ILOC *n1, ILOC *n2) {
+
+    /*
+    ILOC *t = *this;
+
+    while (this->prev) {
+        t = t->prev;
+    }
+
+    t->prev = n1;
+    n1->next = *this;
+    */
+
+}
+
+void print(ILOC *in) {
+
+}
