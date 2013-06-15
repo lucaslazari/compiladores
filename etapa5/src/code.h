fa1246429f030c8c2a463c87b287cf65e994b172
Lucas Lazari <lucaslazari86@gmail.com>
2013-06-15 02:23:01 -0300

    mais mudanças...

---
+++ etapa5/src/code.h
@@ -0,0 +1,22 @@
+#ifndef CODE_H
+#define CODE_H
+
+#include <string>
+#include "tree/common.h"
+#include "tree/node.h"
+
+
+class Code {
+
+    public:
+        Code();
+        static Node * generateCode(Node * root);
+
+    private:
+
+
+
+
+};
+
+#endif // CODE_H
